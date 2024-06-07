5b2bed23e69109c157d0c19220715b6258494696
Richard Addo <ramankwah03@gmail.com>
2024-06-07 22:35:48 +0200

    action added

--- components/containers/cards.tsx
+++ components/containers/cards.tsx
@@ -97,52 +97,8 @@ export default function CommentCard() {
             </div>
           )}
           <div className="border-l md:border-l-0 pl-6 mt-6">
-            <ReplyCard />
-            <ReplyCard />
-          </div>
-        </div>
-      </article>
-      <article className="mx-6 pb-6">
-        <div className="flex flex-row mb-4 items-center justify-between">
-          <div className="flex gap-4 md:gap-8">
-            <Image
-              alt="down"
-              src="/assets/user-images/image-anne.jpg"
-              className="rounded-full"
-              width={40}
-              height={40}
-            />
-            <div>
-              <h1 className="text-[13px] md:text-[14px] text-secondary-light-blue font-bold">
-                Elijah Moss
-              </h1>
-              <span className="text-[13px] md:text-[14px] text-secondary-light-blue">
-                @hexagon.bestagon
-              </span>
-            </div>
-          </div>
-          <button
-            onClick={() => setDisplayReply(!displayReply)}
-            className="text-[13px] text-primary-light-blue font-semibold"
-          >
-            Reply
-          </button>
-        </div>
-        <div className="md:border-l md:ml-[20px] ">
-          <p className="text-[13px] md:ml-[56px] md:text-[15px] text-secondary-light-blue ">
-            Also, please allow styles to be applied based on system preferences.
-            I would love to be able to browse Frontend Mentor in the evening
-            after my device’s dark mode turns on without the bright background
-            it currently has.
-          </p>
-          {displayReply && (
-            <div className="ml-[56px]">
-              <ReplyForm />
-            </div>
-          )}
-          <div className="border-l md:border-l-0 pl-6 mt-6">
-            <ReplyCard />
-            <ReplyCard />
+            {/* <ReplyCard />
+            <ReplyCard /> */}
           </div>
         </div>
       </article>
--- components/containers/comment-container.tsx
+++ components/containers/comment-container.tsx
@@ -1,21 +1,10 @@
 "use client";
 import FeedbackContainer from "./feedback";
-import CommentCard from "./comment-card";
+import CommentCard from "./cards";
 import { useState } from "react";
 import { CommentForm } from "./forms";
 
 export default function CommentContainer() {
-  const [count, setCount] = useState(250);
-  function handleChange(
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const { value } = e.target;
-    if (count == 0) {
-      return;
-    } else {
-      setCount(250 - value.length);
-    }
-  }
   return (
     <section>
       <FeedbackContainer />
@@ -24,34 +13,6 @@ export default function CommentContainer() {
         <CommentCard />
       </section>
 
-      {/* <form className="bg-white mx-6 my-6 rounded-lg" action="">
-        <h1 className="py-6 mx-6 font-bold text-[18px] text-secondary-dark-gray">
-          Add Comment
-        </h1>
-
-        <div className="mx-6 mb-[16px]">
-          <textarea
-            onChange={handleChange}
-            name="comment"
-            maxLength={50}
-            rows={5}
-            placeholder="Type your comment here"
-            className="w-full pl-4 pt-4  placeholder:pt-2 text-secondary-light-blue text-[13px] bg-secondary-very-gray rounded-lg focus:outline-tetiary-sea-blue"
-          />
-        </div>
-
-        <div className="mx-6 flex justify-between items-center pb-6">
-          <span className="text-[13px] text-secondary-light-blue">
-            {count} Characters Left
-          </span>
-          <button
-            className="rounded-lg text-[13px] font-bold bg-primary-voilet py-[10px] px-4 text-white"
-            type="submit"
-          >
-            Post Comment
-          </button>
-        </div>
-      </form> */}
       <CommentForm />
     </section>
   );
